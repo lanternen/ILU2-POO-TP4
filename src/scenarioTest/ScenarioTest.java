7b072625da16cb3826924120e3a3bd4c7bc9c586
lanternen <leo.audouin@univ-tlse3.fr>
2024-04-03 11:40:55 +0200

    TP4 POO. Partie 2 terminée. Oui, y a pas grand chose d'écrit en plus

--- src/scenarioTest/ScenarioTest.java
+++ src/scenarioTest/ScenarioTest.java
@@ -6,15 +6,14 @@ import produit.*;
 public class ScenarioTest {
 	public static void main(String[] args) {
 		
-		IEtal[] marche = new IEtal[3];
+		IEtal<Sanglier>[] marche = new IEtal[3];
 		IEtal<Sanglier> etalSanglier = new Etal<>();
 		IEtal<Poisson> etalPoisson = new Etal<>();
 		marche[0] = etalSanglier;
 		marche[1] = etalPoisson;
 		
 		
-		
-		
+				
 		
 		
 	}
